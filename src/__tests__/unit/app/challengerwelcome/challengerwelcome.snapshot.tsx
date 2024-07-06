5cfa819889712246c36aba9f786ce9a2bb9647d2
Joseph Dvorak <dvorakjt@gmail.com>
2024-07-06 15:25:00 -0400

    added tests for Alert

--- src/__tests__/unit/app/challengerwelcome/challengerwelcome.snapshot.tsx
+++ src/__tests__/unit/app/challengerwelcome/challengerwelcome.snapshot.tsx
@@ -1,15 +1,23 @@
 import ChallengerWelcome from '@/app/challengerwelcome/page';
+import { UserContext, type UserContextType } from '@/contexts/user-context';
 import { cleanup, render } from '@testing-library/react';
 
 jest.mock('next/navigation', () => ({
-  useRouter: jest.fn(),
+  useRouter: () => ({
+    prefetch: jest.fn(),
+    push: jest.fn(),
+  }),
 }));
 
 describe('ChallengerWelcome', () => {
   afterEach(cleanup);
 
   it('renders challenger-welcome page unchanged', () => {
-    const { container } = render(<ChallengerWelcome />);
+    const { container } = render(
+      <UserContext.Provider value={{ user: null } as UserContextType}>
+        <ChallengerWelcome />
+      </UserContext.Provider>,
+    );
     expect(container).toMatchSnapshot();
   });
 });
--- src/__tests__/unit/app/challengerwelcome/challengerwelcome.test.tsx
+++ src/__tests__/unit/app/challengerwelcome/challengerwelcome.test.tsx
@@ -1,4 +1,5 @@
 import ChallengerWelcome from '@/app/challengerwelcome/page';
+import { UserContext, type UserContextType } from '@/contexts/user-context';
 import '@testing-library/jest-dom';
 import { cleanup, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
@@ -9,10 +10,14 @@ jest.mock('next/navigation', () => require('next-router-mock'));
 describe('ChallengerWelcome', () => {
   afterEach(cleanup);
   it('opens the signup page when the first button is clicked', async () => {
-    mockRouter.push('/initial-path');
+    mockRouter.push('/challengerwelcome');
     const user = userEvent.setup();
 
-    render(<ChallengerWelcome />);
+    render(
+      <UserContext.Provider value={{ user: null } as UserContextType}>
+        <ChallengerWelcome />
+      </UserContext.Provider>,
+    );
 
     const fgetStartedbtn = screen.getAllByRole('button')[0];
     await user.click(fgetStartedbtn);
@@ -23,8 +28,11 @@ describe('ChallengerWelcome', () => {
     mockRouter.push('/initial-path');
     const user = userEvent.setup();
 
-    render(<ChallengerWelcome />);
-
+    render(
+      <UserContext.Provider value={{ user: null } as UserContextType}>
+        <ChallengerWelcome />
+      </UserContext.Provider>,
+    );
     const sgetStartedbtn = screen.getAllByRole('button')[1];
     await user.click(sgetStartedbtn);
     expect(mockRouter).toMatchObject({ asPath: '/signup' });
