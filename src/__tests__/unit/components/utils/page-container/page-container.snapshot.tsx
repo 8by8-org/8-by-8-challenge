23de7a203e648f1b1013a5ca7f0bbb1cf486b5df
Noreen Sulehry <noreensulehry1@gmail.com>
2024-07-26 12:39:09 -0500

    CHAL-16: Fixed page height

--- src/__tests__/unit/components/utils/page-container/page-container.snapshot.tsx
+++ src/__tests__/unit/components/utils/page-container/page-container.snapshot.tsx
@@ -8,4 +8,13 @@ describe('PageContainer', () => {
     const { container } = render(<PageContainer />);
     expect(container).toMatchSnapshot();
   });
+  it('applies the correct min-height style.', () => {
+    const { getByTestId } = render(<PageContainer />);
+    const pageContainer = getByTestId('page-container');
+    const navbarHeight = '57px'; 
+    const footerHeight = '60px'; //footer padding 30x top and bottom
+    const expectedMinHeight = `calc(100vh - ${navbarHeight} - ${footerHeight})`;
+
+    expect(pageContainer).toHaveStyle(`min-height: ${expectedMinHeight}`);
+  });
 });
