337f2c581d8772d77c27c55672be83a354273703
Noreen Sulehry <noreensulehry1@gmail.com>
2024-07-26 13:20:42 -0500

    CHAL-16: Fixed page height

--- src/__tests__/unit/components/utils/page-container/page-container.snapshot.tsx
+++ src/__tests__/unit/components/utils/page-container/page-container.snapshot.tsx
@@ -4,16 +4,20 @@ import { PageContainer } from '@/components/utils/page-container';
 
 describe('PageContainer', () => {
   afterEach(cleanup);
+
   it('renders the PageContainer unchanged.', () => {
     const { container } = render(<PageContainer />);
     expect(container).toMatchSnapshot();
   });
+
   it('applies the correct min-height style.', () => {
     const { getByTestId } = render(<PageContainer />);
     const pageContainer = getByTestId('page-container');
-    const navbarHeight = '57px'; 
-    const footerHeight = '60px'; //footer padding 30x top and bottom
+    const navbarHeight = '57px'; // Adjust this 
+    const footerHeight = '40px'; // Adjust 
     const expectedMinHeight = `calc(100vh - ${navbarHeight} - ${footerHeight})`;
+
+    console.log('Rendered min-height:', pageContainer.style.minHeight);
 
     expect(pageContainer).toHaveStyle(`min-height: ${expectedMinHeight}`);
   });
