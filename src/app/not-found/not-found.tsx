0431941ff42eeb16af0855a8367d17205de66f43
Phillip D <p.graham.doran@gmail.com>
2024-07-29 15:26:14 -0400

    Stage commit for 404 page

---
+++ src/app/not-found/not-found.tsx
@@ -0,0 +1,9 @@
+import { PageContainer } from '@/components/utils/page-container';
+
+export default function NotFound() {
+    return (
+        <PageContainer>
+
+        </PageContainer>
+    );
+}
