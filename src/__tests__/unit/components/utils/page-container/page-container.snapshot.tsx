db205cd5843a33f91fcda95f95015db87ead0d0b
Noreen Sulehry <noreensulehry1@gmail.com>
2024-08-02 12:03:17 -0500

    removed the page container test

--- src/__tests__/unit/components/utils/page-container/page-container.snapshot.tsx
+++ src/__tests__/unit/components/utils/page-container/page-container.snapshot.tsx
@@ -9,16 +9,4 @@ describe('PageContainer', () => {
     const { container } = render(<PageContainer />);
     expect(container).toMatchSnapshot();
   });
-
-  it('applies the correct min-height style.', () => {
-    const { getByTestId } = render(<PageContainer />);
-    const pageContainer = getByTestId('page-container');
-    const navbarHeight = '57px'; // Adjust this 
-    const footerHeight = '40px'; // Adjust 
-    const expectedMinHeight = `calc(100vh - ${navbarHeight} - ${footerHeight})`;
-
-    console.log('Rendered min-height:', pageContainer.style.minHeight);
-
-    expect(pageContainer).toHaveStyle(`min-height: ${expectedMinHeight}`);
-  });
 });
