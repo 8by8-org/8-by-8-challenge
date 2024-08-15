96538ad680ad556c52ffe0d80f2273a3ac9d1624
Noreen Sulehry <noreensulehry1@gmail.com>
2024-08-15 14:13:57 -0500

    Adding take challenge button

--- src/__tests__/unit/app/page.snapshot.tsx
+++ src/__tests__/unit/app/page.snapshot.tsx
@@ -1,13 +1,17 @@
-import { render, cleanup } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import { mockDialogMethods } from '@/utils/test/mock-dialog-methods';
 import Home from '@/app/page';
 
-describe('Home', () => {
+// describe('Home', () => {
+describe('Home Page - Take the Challenge Button', () => {
   mockDialogMethods();
   afterEach(cleanup);
 
-  it('renders homepage unchanged', () => {
+ // it('renders homepage unchanged', () => {
+  it('renders the "Take the Challenge" button unchanged', () => {
     const { container } = render(<Home />);
+    const challengeButton = screen.getByRole('button', { name: /Take the Challenge/i });
+    expect(challengeButton).toBeInTheDocument();
     expect(container).toMatchSnapshot();
   });
 });
