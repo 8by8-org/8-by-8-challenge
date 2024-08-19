c51c2867208aab016344990c16227e2d090eef78
Joseph Dvorak <dvorakjt@gmail.com>
2024-08-19 19:47:05 -0400

    removed commented out tests

--- src/__tests__/unit/components/progress/badges/badges.test.tsx
+++ src/__tests__/unit/components/progress/badges/badges.test.tsx
@@ -9,35 +9,6 @@ import type { PlayerBadge } from '@/model/types/player-badge';
 describe('Bagdes', () => {
   afterEach(cleanup);
 
-  // it('does not modify the original badges array.', () => {
-  //   const badges: Badge[] = [
-  //     { action: Actions.VoterRegistration },
-  //     { action: Actions.SharedChallenge },
-  //     { playerName: 'test', playerAvatar: '1' },
-  //   ];
-  //   render(<Badges badges={badges} />);
-
-  //   expect(badges).toHaveLength(3);
-  // });
-
-  // it('does not modify the original badges array', () => {
-  //   const badges: Badge[] = [
-  //     { action: Actions.VoterRegistration },
-  //     { action: Actions.SharedChallenge },
-  //     { playerName: 'test', playerAvatar: '1' },
-  //     { playerName: 'test1', playerAvatar: '1' },
-  //     { playerName: 'test2', playerAvatar: '1' },
-  //     { playerName: 'test3', playerAvatar: '1' },
-  //     { playerName: 'test4', playerAvatar: '1' },
-  //     { playerName: 'test5', playerAvatar: '1' },
-  //     { playerName: 'test6', playerAvatar: '1' },
-  //     { playerName: 'test7', playerAvatar: '1' },
-  //   ];
-  //   render(<Badges badges={badges} />);
-
-  //   expect(badges).toHaveLength(10);
-  // });
-
   it('renders action badges.', () => {
     const actionBadges = [
       {
