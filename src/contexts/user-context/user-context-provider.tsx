6319e49ee33f7593357df440d3787dd8272b0bfc
Joseph Dvorak <dvorakjt@gmail.com>
2024-09-07 23:35:03 -0400

    added InvitationsRepository

--- src/contexts/user-context/user-context-provider.tsx
+++ src/contexts/user-context/user-context-provider.tsx
@@ -2,7 +2,6 @@ import 'server-only';
 import { serverContainer } from '@/services/server/container';
 import { SERVER_SERVICE_KEYS } from '@/services/server/keys';
 import { ClientSideUserContextProvider } from './client-side-user-context-provider';
-import { cookies as nextCookies } from 'next/headers';
 import type { PropsWithChildren } from 'react';
 
 /**
@@ -14,9 +13,6 @@ export async function UserContextProvider({ children }: PropsWithChildren) {
   const auth = serverContainer.get(SERVER_SERVICE_KEYS.Auth);
   const cookies = serverContainer.get(SERVER_SERVICE_KEYS.Cookies);
   const emailForSignIn = await cookies.loadEmailForSignIn();
-  const inviteCode = cookies.getInviteCode();
-  console.log(inviteCode);
-
   let user = await auth.loadSessionUser();
   let invitedBy = null;
 
