7226daaad6dbfdc35df813cc1e477cd6ad620d67
Joseph Dvorak <dvorakjt@gmail.com>
2024-09-19 03:54:06 -0400

    removed superfluous files

--- src/services/server/us-state-information/rtv-state-requirements-response-schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const rockTheVoteStateRequirementsResponseSchema = z.object({
-  no_party_msg: z.string({ required_error: 'no_party_msg is required.' }),
-  party_list: z.array(z.string()),
-  race_list: z.array(z.string()),
-  id_number_msg: z.string({ required_error: 'id_number_msg is required.' }),
-});
