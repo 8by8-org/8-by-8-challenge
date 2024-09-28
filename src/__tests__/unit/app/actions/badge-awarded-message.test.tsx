cb8812ad1b7a10fdd4655a57d052c451af81fe3f
Joseph Dvorak <dvorakjt@gmail.com>
2024-09-28 03:48:05 -0400

    added unit tests for AvailableActions

--- src/__tests__/unit/app/actions/badge-awarded-message.test.tsx
+++ src/__tests__/unit/app/actions/badge-awarded-message.test.tsx
@@ -47,4 +47,119 @@ describe('BadgeAwardedMessage', () => {
       ),
     ).toBeInTheDocument();
   });
+
+  it('does not display a message if the user is signed out.', () => {
+    const challengerData: ChallengerData = {
+      challengerName: 'Challenger',
+      challengerInviteCode: createId(),
+      challengerAvatar: '0',
+    };
+
+    render(
+      <UserContext.Provider
+        value={Builder<UserContextType>()
+          .user(null)
+          .invitedBy(challengerData)
+          .build()}
+      >
+        <BadgeAwardedMessage />
+      </UserContext.Provider>,
+    );
+
+    expect(screen.queryByText(/got a badge/i)).not.toBeInTheDocument();
+  });
+
+  it(`does not display a message if the user has not been invited by a 
+  challenger.`, () => {
+    const user = Builder<User>()
+      .type(UserType.Player)
+      .contributedTo([])
+      .completedActions({
+        electionReminders: false,
+        registerToVote: false,
+        sharedChallenge: false,
+      })
+      .build();
+
+    render(
+      <UserContext.Provider
+        value={Builder<UserContextType>().user(user).invitedBy(null).build()}
+      >
+        <BadgeAwardedMessage />
+      </UserContext.Provider>,
+    );
+
+    expect(screen.queryByText(/got a badge/i)).not.toBeInTheDocument();
+  });
+
+  it(`does not display a message if the user has completed all actions that 
+  would award an inviting challenger a badge.`, () => {
+    const challengerData: ChallengerData = {
+      challengerName: 'Challenger',
+      challengerInviteCode: createId(),
+      challengerAvatar: '0',
+    };
+
+    const user = Builder<User>()
+      .type(UserType.Hybrid)
+      .contributedTo([challengerData])
+      .completedActions({
+        electionReminders: true,
+        registerToVote: true,
+        sharedChallenge: false,
+      })
+      .build();
+
+    render(
+      <UserContext.Provider
+        value={Builder<UserContextType>()
+          .user(user)
+          .invitedBy(challengerData)
+          .build()}
+      >
+        <BadgeAwardedMessage />
+      </UserContext.Provider>,
+    );
+
+    expect(screen.queryByText(/got a badge/i)).not.toBeInTheDocument();
+  });
+
+  it(`does not display a message if the last challenger that the user 
+  contributed to is not the challenger whose invitation they most recently 
+  visited.`, () => {
+    const challengerA: ChallengerData = {
+      challengerName: 'Challenger A',
+      challengerInviteCode: createId(),
+      challengerAvatar: '0',
+    };
+
+    const challengerB: ChallengerData = {
+      challengerName: 'Challenger B',
+      challengerInviteCode: createId(),
+      challengerAvatar: '1',
+    };
+
+    const user = Builder<User>()
+      .type(UserType.Player)
+      .contributedTo([challengerA])
+      .completedActions({
+        electionReminders: false,
+        registerToVote: false,
+        sharedChallenge: false,
+      })
+      .build();
+
+    render(
+      <UserContext.Provider
+        value={Builder<UserContextType>()
+          .user(user)
+          .invitedBy(challengerB)
+          .build()}
+      >
+        <BadgeAwardedMessage />
+      </UserContext.Provider>,
+    );
+
+    expect(screen.queryByText(/got a badge/i)).not.toBeInTheDocument();
+  });
 });
