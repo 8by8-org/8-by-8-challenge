d911e106dad0c53893c948fb36ee1943e2890785
Phillip D <p.graham.doran@gmail.com>
2024-09-30 17:58:49 -0400

    Added functionality for the PlayerWelcome page

--- src/app/playerwelcome/page.tsx
+++ src/app/playerwelcome/page.tsx
@@ -1,6 +1,9 @@
 'use client';
 import { PageContainer } from '@/components/utils/page-container';
 import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 import { UserContext } from '@/contexts/user-context';
 import { useContextSafely } from '@/hooks/use-context-safely';
 import styles from './styles.module.scss';
@@ -8,72 +11,111 @@ import { Button } from '@/components/utils/button';
 
 export default function PlayerWelcome() {
   const { invitedBy } = useContextSafely(UserContext, 'UserContext');
-  const challengerName = invitedBy?.challengerName ?? 'your challenger';
+  const challengerName =
+    invitedBy?.challengerName ? `${invitedBy.challengerName}'s` : 'the';
+  const subtitleText =
+    invitedBy ?
+      `Help ${invitedBy.challengerName} win their 8by8 Challenge by registering to vote or taking other actions to #stopasianhate!`
+    : 'Help support the AAPI community by registering to vote or taking other actions to #stopasianhate!';
+
+  const router = useRouter();
+  useEffect(() => {
+    router.prefetch('/signup');
+  }, [router]);
 
   return (
     <PageContainer>
-      <div className={styles.maincontent}>
-        <span>
-          <h1>Support {challengerName}'s 8by8 Challenge!</h1>
-        </span>
-        <p>
-          Help {challengerName} win their 8by8 Challenge by registering to vote
-          or taking other actions to #stopasianhate!
+      <div className={styles.banner}>
+        <h1 className="mt_md mb_md">
+          <span className="underline">Support</span> {challengerName}
+          <br /> 8by8 Challenge!
+        </h1>
+        <p className={styles.subtitle}>{subtitleText}</p>
+        <Button
+          size="lg"
+          wide
+          onClick={() => {
+            router.push('/signup');
+          }}
+        >
+          Get started
+        </Button>
+        <p className={styles.signin_link}>
+          Already have an account?{' '}
+          <Link href="/signin" className="link--teal">
+            Sign In
+          </Link>
         </p>
-        <Button className={styles.Button}>Get started</Button>
-        <span className={styles.signin_link}>
-          Already have an account? Sign In
-        </span>
+      </div>
 
-        <div id="welcomecolumn">
-          <h1 className={styles.centeredtext}>Here's how it works</h1>
-          <h2>1. Choose an action to take</h2>
-          <p>
+      <div className={styles.curve}></div>
+
+      <div className={styles.explanation}>
+        <h1 className={styles.title}>
+          <span className="underline">Here's how it works</span>
+        </h1>
+        <div>
+          <h3>1. Choose an action to take</h3>
+          <p className={styles.b2_extend}>
             You can take any number of available actions: register to vote, get
             election reminders, or take the 8by8 challenge yourself. Pick one to
             start.
           </p>
-          <Image
-            src="/static/images/pages/player-welcome/person-with-take-action-sign.png"
-            width={120}
-            height={120}
-            alt="person holding a take action sign"
-            className={styles.person_with_sign}
-          />
-          <br />
+          <div className={styles.images}>
+            <Image
+              src="/static/images/pages/playerwelcome/person-with-take-action-sign.png"
+              width={206}
+              height={139}
+              alt="person holding a take action sign"
+            />
+          </div>
 
-          <h2>2. Your friend will earn a badge</h2>
-          <p>
+          <h3>2. Your friend will earn a badge</h3>
+          <p className={styles.b2_extend}>
             Any of the 3 actions will helf your friend earn a badge, and get
             closer to winning the challenge.
           </p>
-          <Image
-            src="/static/images/pages/player-welcome/cell-phone.png"
-            width={120}
-            height={120}
-            alt="cell phone with a crown image on the screen"
-            className={styles.cell_phone}
-          />
-          <br />
+          <div className={styles.images}>
+            <Image
+              src="/static/images/pages/playerwelcome/cell-phone.png"
+              width={128}
+              height={127}
+              alt="cell phone with a crown image on the screen"
+            />
+          </div>
 
-          <h2>3. Come back and take more actions</h2>
-          <p>
+          <h3>3. Come back and take more actions</h3>
+          <p className={styles.b2_extend}>
             Whether it is to help the same friend or a different one, the more
             action you take, the better! Note that you can only earn one badge
             per friend.
           </p>
-          <Image
-            src="/static/images/pages/player-welcome/person-with-we-need-your-help-sign.png"
-            width={120}
-            height={120}
-            alt="person holding a sign saying we need your help"
-            className={styles.help_sign}
-          />
-          <br />
+          <div className={styles.image_last}>
+            <Image
+              src="/static/images/pages/playerwelcome/person-with-we-need-your-help-sign.png"
+              width={141}
+              height={141}
+              alt="person holding a sign saying we need your help"
+            />
+          </div>
 
           <div>
-            <Button className={styles.Button}>Get started</Button>
+            <Button
+              size="lg"
+              wide
+              onClick={() => {
+                router.push('/signup');
+              }}
+            >
+              Get started
+            </Button>
           </div>
+          <p className={styles.signin_link}>
+            Already have an account?{' '}
+            <Link href="/signin" className="link">
+              Sign In
+            </Link>
+          </p>
         </div>
       </div>
     </PageContainer>
