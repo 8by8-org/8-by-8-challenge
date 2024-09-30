26fe69765df46f9a9b82cc59857df8da6a267c80
Joseph Dvorak <dvorakjt@gmail.com>
2024-09-29 22:53:18 -0400

    add tests for lastContributedToCurrentInviter, format and fix linting errors

--- src/app/actions/utils/last-contributed-to-current-inviter.ts
+++ src/app/actions/utils/last-contributed-to-current-inviter.ts
@@ -1,10 +1,11 @@
 import type { ChallengerData } from '@/model/types/challenger-data';
 
 export function lastContributedToCurrentInviter(
-  contributedTo: ChallengerData[] = [],
+  contributedTo: ChallengerData[] | undefined,
   invitedBy: ChallengerData,
 ): boolean {
   return (
+    !!contributedTo &&
     !!contributedTo.length &&
     contributedTo.at(-1)?.challengerInviteCode ===
       invitedBy.challengerInviteCode
